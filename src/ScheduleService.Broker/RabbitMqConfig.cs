b72b35dfe678c7ac8e23c360a5c13185714557fd
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2023-12-07 00:27:08 +0300

    add initial structure

---
+++ src/ScheduleService.Broker/RabbitMqConfig.cs
@@ -0,0 +1,14 @@
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+
+namespace LT.DigitalOffice.ScheduleService.Broker;
+
+public class RabbitMqConfig : BaseRabbitMqConfig
+{
+  #region receive endpoints
+
+  #endregion
+
+  #region request endpoints
+  
+  #endregion
+}
