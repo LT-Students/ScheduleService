ebe5f6467f650d2d007ad77abf0586228de7ec73
Marat Hidirov <hidiromarat@gmail.com>
2023-12-16 19:34:12 +0300

    fix Bisiness.Group

--- src/ScheduleService.Business/Group/FindGroupCommand.cs
+++ src/ScheduleService.Business/Group/FindGroupCommand.cs
@@ -1,5 +1,5 @@
 ﻿using LT.DigitalOffice.Kernel.Responses;
-using LT.DigitalOffice.ScheduleService.Business.Groups.Interfaces;
+using LT.DigitalOffice.ScheduleService.Business.Group.Interfaces;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.Group;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Responses.Group;
 using System.Threading.Tasks;
