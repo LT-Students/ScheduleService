e9dd0562ff2a195df9de8fb1df5e11f33c8b6350
Marat Hidirov <hidiromarat@gmail.com>
2023-12-16 19:30:51 +0300

    add 'AutoInject' to all services

--- src/ScheduleService.Business/Group/Interfaces/ICreateGroupCommand.cs
+++ src/ScheduleService.Business/Group/Interfaces/ICreateGroupCommand.cs
@@ -2,9 +2,11 @@
 using System.Threading.Tasks;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.Group;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Responses.Group;
+using LT.DigitalOffice.Kernel.Attributes;
 
 namespace LT.DigitalOffice.ScheduleService.Business.Group.Interfaces;
 
+[AutoInject]
 public interface ICreateGroupCommand
 {
   Task<OperationResultResponse<CreateGroupResponse>> ExecuteAsync(CreateGroupRequest request);
--- src/ScheduleService.Business/Group/Interfaces/IEditGroupCommand.cs
+++ src/ScheduleService.Business/Group/Interfaces/IEditGroupCommand.cs
@@ -1,10 +1,12 @@
-﻿using LT.DigitalOffice.Kernel.Responses;
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.Responses;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.Group;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Responses.Group;
 using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.ScheduleService.Business.Group.Interfaces;
 
+[AutoInject]
 public interface IEditGroupCommand
 {
   Task<OperationResultResponse<EditGroupResponse>> ExecuteAsync(EditGroupRequest request);
