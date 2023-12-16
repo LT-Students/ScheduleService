b6628e6c0b250a6000c90d0bb837f314c8879059
Marat Hidirov <hidiromarat@gmail.com>
2023-12-16 17:12:29 +0300

    Add group to validator

---
+++ src/ScheduleService.Validation/Group/Interfaces/IUpdateGroupRequestValidator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.Group;
+
+namespace LT.DigitalOffice.ScheduleService.Validation.Group.Interfaces;
+
+public interface IUpdateGroupRequestValidator : IValidator<UpdateGroupRequest>
+{
+}
---
+++ src/ScheduleService.Validation/Group/UpdateGroupRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.Group;
+using LT.DigitalOffice.ScheduleService.Validation.Group.Interfaces;
+
+namespace LT.DigitalOffice.ScheduleService.Validation.Groups;
+
+public class UpdateGroupRequestValidator : AbstractValidator<UpdateGroupRequest>, IUpdateGroupRequestValidator
+{
+  public UpdateGroupRequestValidator() { }
+}
