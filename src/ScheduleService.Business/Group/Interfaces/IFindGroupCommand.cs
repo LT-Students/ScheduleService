41dd0bb20492196dddcaa5ef348b74c57a3ea603
Marat Hidirov <hidiromarat@gmail.com>
2023-12-16 18:22:17 +0300

    add GropsController.cs to Controller

--- src/ScheduleService.Business/Group/Interfaces/IFindGroupCommand.cs
+++ src/ScheduleService.Business/Group/Interfaces/IFindGroupCommand.cs
@@ -3,7 +3,7 @@ using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.Group;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Responses.Group;
 using System.Threading.Tasks;
 
-namespace LT.DigitalOffice.ScheduleService.Business.Groups.Interfaces;
+namespace LT.DigitalOffice.ScheduleService.Business.Group.Interfaces;
 
 public interface IFindGroupCommand
 {
--- src/ScheduleService/Controllers/GroupsController.cs
+++ src/ScheduleService/Controllers/GroupsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.ScheduleService.Business.Group.Interfaces;
+using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.Group;
+using LT.DigitalOffice.ScheduleService.Models.Dto.Responses.Group;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.ScheduleService.Controllers;
@@ -7,10 +11,51 @@ namespace LT.DigitalOffice.ScheduleService.Controllers;
 [ApiController]
 public class GroupsController
 {
-  [HttpPost("create")]
-  public async Task<IActionResult> CreateAsync(
-    [FromServices] ICreate)
+  [HttpPost]
+  public async Task<OperationResultResponse<CreateGroupResponse>> CreateAsync(
+    [FromServices] ICreateGroupCommand command,
+    [FromBody] CreateGroupRequest request)
   {
+    return await command.ExecuteAsync(request);
+  }
 
+  [HttpGet]
+  public async Task<OperationResultResponse<FindGroupResponse>> FindAsync(
+    [FromServices] IFindGroupCommand command,
+    [FromBody] FindGroupRequest request)
+  {
+    return await command.ExecuteAsync(request);
+  }
+
+  [HttpGet("findAll")]
+  public async Task<OperationResultResponse<FindGroupsResponse>> CreateAsync(
+    [FromServices] IFindGroupsCommand command,
+    [FromBody] FindGroupsRequest request)
+  {
+    return await command.ExecuteAsync(request);
+  }
+
+  [HttpDelete]
+  public async Task<OperationResultResponse<DeleteGroupResponse>> CreateAsync(
+    [FromServices] IDeleteGroupCommand command,
+    [FromBody] DeleteGroupRequest request)
+  {
+    return await command.ExecuteAsync(request);
+  }
+
+  [HttpPatch]
+  public async Task<OperationResultResponse<UpdateGroupResponse>> CreateAsync(
+  [FromServices] IUpdateGroupCommand command,
+  [FromBody] UpdateGroupRequest request)
+  {
+    return await command.ExecuteAsync(request);
+  }
+
+  [HttpPut]
+  public async Task<OperationResultResponse<EditGroupResponse>> CreateAsync(
+  [FromServices] IEditGroupCommand command,
+  [FromBody] EditGroupRequest request)
+  {
+    return await command.ExecuteAsync(request);
   }
 }
