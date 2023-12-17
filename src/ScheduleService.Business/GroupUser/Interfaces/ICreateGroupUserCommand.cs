dae6843e94f17bfb9feb437d6f44e9af7f206104
Marat Hidirov <hidiromarat@gmail.com>
2023-12-17 18:50:35 +0300

    add  AutoInject

--- src/ScheduleService.Business/GroupUser/Interfaces/ICreateGroupUserCommand.cs
+++ src/ScheduleService.Business/GroupUser/Interfaces/ICreateGroupUserCommand.cs
@@ -2,9 +2,11 @@
 using System.Threading.Tasks;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.GroupUser;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Responses.GroupUser;
+using LT.DigitalOffice.Kernel.Attributes;
 
 namespace LT.DigitalOffice.ScheduleService.Business.GroupUser.Interfaces;
 
+[AutoInject]
 public interface ICreateGroupUserCommand
 {
   Task<OperationResultResponse<CreateGroupUserResponse>> ExecuteAsync(CreateGroupUserRequest request);
--- src/ScheduleService.Business/GroupUser/Interfaces/IFindGroupsUsersCommand.cs
+++ src/ScheduleService.Business/GroupUser/Interfaces/IFindGroupsUsersCommand.cs
@@ -1,10 +1,12 @@
-﻿using LT.DigitalOffice.Kernel.Responses;
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.Responses;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.GroupUser;
 using LT.DigitalOffice.ScheduleService.Models.Dto.Responses.GroupUser;
 using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.ScheduleService.Business.GroupUser.Interfaces;
 
+[AutoInject]
 public interface IFindGroupsUsersCommand
 {
   Task<OperationResultResponse<FindGroupsUsersResponse>> ExecuteAsync(FindGroupsUsersRequest request);
