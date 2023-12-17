e908240564e75de0f56a6c2e172723780650f330
Marat Hidirov <hidiromarat@gmail.com>
2023-12-17 15:57:38 +0300

    add groupUser to Validation

---
+++ src/ScheduleService.Validation/GroupUser/Interfaces/IUpdateGroupUserRequestValidator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.GroupUser;
+
+namespace LT.DigitalOffice.ScheduleService.Validation.GroupUser.Interfaces;
+
+public interface IUpdateGroupUserRequestValidator : IValidator<UpdateGroupUserRequest>
+{
+}
---
+++ src/ScheduleService.Validation/GroupUser/UpdateGroupUserRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.ScheduleService.Models.Dto.Requests.GroupUser;
+using LT.DigitalOffice.ScheduleService.Validation.GroupUser.Interfaces;
+
+namespace LT.DigitalOffice.ScheduleService.Validation.GroupUser;
+
+public class UpdateGroupUserRequestValidator : AbstractValidator<UpdateGroupUserRequest>, IUpdateGroupUserRequestValidator
+{
+  public UpdateGroupUserRequestValidator()
+  {
+  }
+}
