df2ea819e7c700bdd1c2db0e07028fecd083298b
Marina Mel <melm2004@mail.ru>
2023-12-22 13:53:31 +0300

    delete extra properties

--- src/ScheduleService.Models.Db/DbGroupsUser.cs
+++ src/ScheduleService.Models.Db/DbGroupsUser.cs
@@ -10,8 +10,6 @@ public class DbGroupsUser
 
   public Guid GroupId { get; set; }
   public Guid UserId { get; set; }
-  public Guid CreatedBy { get; set; }
-  public DateTime CreatedAtUtc { get; set; }
 
   public DbGroup Group { get; set; }
 }
