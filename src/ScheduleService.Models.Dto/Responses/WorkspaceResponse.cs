8aca31a919822ac75f2f3433d4cc93a5280d8705
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2024-02-29 21:29:59 +0300

    Edit workspace response

--- src/ScheduleService.Models.Dto/Responses/WorkspaceResponse.cs
+++ src/ScheduleService.Models.Dto/Responses/WorkspaceResponse.cs
@@ -4,7 +4,18 @@ namespace LT.DigitalOffice.ScheduleService.Models.Dto.Responses;
 
 public record WorkspaceResponse
 {
+  /// <summary>
+  /// Workspace identifier.
+  /// </summary>
   public Guid Id { get; set; }
+
+  /// <summary>
+  /// Workspace name.
+  /// </summary>
   public string Name { get; set; }
+
+  /// <summary>
+  /// When this value is 'true' the workspace is active, when it's 'false' the workspace is inactive.
+  /// </summary>
   public bool IsActive { get; set; }
 }
